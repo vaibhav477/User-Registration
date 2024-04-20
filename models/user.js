10e1a8da8539c93fa6f6863319858f89984c8115
Vaibhav Aggarwal <vaibhav.aggarwal477@gmail.com>
2024-04-21 02:51:22 +0530

    user model made

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    profile: {
+        location: String,
+        age: Number,
+        workDetails: String
+    }
+});
+
+
+// password hashing
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next(); // Only hash if modified  
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
